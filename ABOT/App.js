32953910e952d53ecfa668c7bf8f24f4fd06dcab
hallieraew <hallieweintraub@gmail.com>
2020-04-28 11:53:46 -0500

    added in code for submit form

--- ABOT/App.js
+++ ABOT/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FormAvailPto from "./component/FormAvailPto";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FormAvailPto />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-evenly',
   },
 });
--- ABOT/component/FormAvailPto.js
+++ ABOT/component/FormAvailPto.js
@@ -1,1 +1,49 @@
 //reference to form already built
+
+import React, { Component } from "react";
+import {
+	Container,
+	Header,
+	Content,
+	Form,
+	Item,
+	Input,
+	Label,
+    Button,
+    Text
+} from "native-base";
+
+export default class FormAvailPto extends Component {
+	render() {
+		return (
+			<Container>
+				<Header />
+				<Content>
+					<Form>
+						<Item stackedLabel>
+							<Label>First Name</Label>
+							<Input />
+						</Item>
+						<Item stackedLabel>
+							<Label>Last Name</Label>
+							<Input />
+						</Item>
+						<Item stackedLabel>
+							<Label>Date</Label>
+							<Input />
+						</Item>
+						<Item stackedLabel>
+							<Label>Start Time</Label>
+							<Input />
+						</Item>
+						<Item stackedLabel>
+							<Label>End Time</Label>
+							<Input />
+						</Item>
+ 
+					</Form>
+				</Content>
+			</Container>
+		);
+	}
+}
