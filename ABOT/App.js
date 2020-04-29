7c1f84b3f67919b562a96f9a61dca5f0243aacbe
Mai Houa Vue <mhvue4@gmail.com>
2020-04-28 22:01:32 -0500

    trying demo form tutorial for react native

--- ABOT/App.js
+++ ABOT/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Form, ErrorBoundary} from "./component/Form";
 
 export default function App() {
   return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    // </View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Form />
     </View>
+     
   );
 }
 
--- ABOT/component/Form.js
+++ ABOT/component/Form.js
@@ -1,1 +1,73 @@
 //form for sign up and login 
+
+import React, {Component} from "react";
+import {Text, View, Picker, TextInput, Button, StyleSheet} from "react-native";
+
+//email and password reuseable code in both signup and login
+export class Form extends Component {
+    state ={
+        email:"",
+        password: "" 
+    }
+
+    //function here to handle change 
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <Text style={styles.formLabel}> Demo Form </Text>
+                <View>
+                <TextInput
+                    style={styles.inputStyle}
+                    name="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                />
+                <TextInput
+                    style={styles.inputStyle}
+                    name="password"
+                    placeholder="password"
+                    value={this.state.password}
+                />
+                </View>
+            </View>
+        )
+    }
+}
+
+// https://blog.logrocket.com/build-better-forms-with-react-native-ui-components/
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 50,
+    },
+  
+    formLabel: {
+      fontSize: 20,
+      color: '#fff',
+    },
+    inputStyle: {
+      marginTop: 20,
+      width: 300,
+      height: 40,
+      paddingHorizontal: 10,
+      borderRadius: 50,
+      backgroundColor: '#DCDCDC',
+    },
+    formText: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#fff',
+      fontSize: 20,
+    },
+    text: {
+      color: '#fff',
+      fontSize: 20,
+    },
+  });
+//signup
+
+//login
