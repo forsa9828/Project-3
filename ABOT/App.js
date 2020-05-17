5e31ca025228711f51163b1e68edebdd9692718e
hallieraew <hallieweintraub@gmail.com>
2020-05-17 15:35:28 -0500

    pages working with navbar

--- ABOT/App.js
+++ ABOT/App.js
@@ -1,31 +1,27 @@
-import React, { Component } from "react";
-import { StyleSheet, Container, ScrollView } from "react-native";
-import { Root, View } from "native-base";
-import * as Font from "expo-font";
-import ReviewPto from "./src/component/ReviewPto";
-// import SchedulePage from "./src/pages/SchedulePage";
-// import RequestOff from "./src/pages/RequestOff";
+import 'react-native-gesture-handler';
+import React, {Component} from "react";
+import { StyleSheet, ScrollView, View, Text } from "react-native";
+import { Root, Container } from "native-base";
+import * as Font from 'expo-font';
 import Avail from "./src/pages/Avail";
-// import ManageEmployees from "./src/pages/ManageEmployees";
-// import EmployeeList from "./src/component/EmployeeList";
-import { NavBar } from "./src/component/Navbar";
+import ManageEmployees from "./src/pages/ManageEmployees";
+import EmployeeList from "./src/component/EmployeeList";
+import NavBar from "./src/component/Navbar";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ManageEmployees from "./src/pages/ManageEmployees";
-import SchedulePage from "./src/pages/SchedulePage";
-import RequestOff from "./src/pages/RequestOff";
-import Review from "./src/pages/Review";
+
 
 
 export default function App() {
   return (
 	  <Root>
-<View style={styles.container}>
-
-<SchedulePage/>
-
-</View>
+	<Container style={styles.container}>
+    <NavigationContainer>
+      <NavBar />
+    </NavigationContainer>
+	</Container>
 	  </Root>
+
   );
 }
 
@@ -33,11 +29,9 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 10,
-		backgroundColor: "#fff",
-		alignItems: "stretch",
-		justifyContent: "space-evenly"
 	},
 	scroll: {
 		marginHorizontal: 5
-	}
-});
+	},
+
+	  });
